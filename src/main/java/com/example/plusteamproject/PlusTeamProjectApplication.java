649ab09e7595b160cd50678c4f89bc221773ec55
chaewon9999 <w0729@naver.com>
2025-05-16 19:05:12 +0900

    feat : create BaseEntity

--- src/main/java/com/example/plusteamproject/PlusTeamProjectApplication.java
+++ src/main/java/com/example/plusteamproject/PlusTeamProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.plusteamproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PlusTeamProjectApplication {
 
 	public static void main(String[] args) {
