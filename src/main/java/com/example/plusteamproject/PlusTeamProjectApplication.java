34458b58508fc002576f1cee2df7337ee0a5db3c
sconers00 <jungnam369@naver.com>
2025-05-16 17:39:52 +0900

    initial

---
+++ src/main/java/com/example/plusteamproject/PlusTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.plusteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusTeamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plusteamproject/PlusTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plusteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusTeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
