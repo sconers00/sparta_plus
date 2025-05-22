313f8c26a6ad42aceb212598b123e3a928d4498a
chaewon9999 <w0729@naver.com>
2025-05-22 17:26:03 +0900

    ref : searchV2 기존 카페인 -> redis로 변경

--- src/main/java/com/example/plusteamproject/domain/searchV2/service/SearchServiceV2.java
+++ src/main/java/com/example/plusteamproject/domain/searchV2/service/SearchServiceV2.java
@@ -4,40 +4,41 @@ import com.example.plusteamproject.domain.product.dto.ProductResponseDto;
 import com.example.plusteamproject.domain.product.entity.Product;
 import com.example.plusteamproject.domain.product.repository.ProductRepository;
 import com.example.plusteamproject.domain.searchV1.dto.PopularSearchResponseDto;
-import com.example.plusteamproject.domain.searchV2.entity.SearchV2;
-import com.example.plusteamproject.domain.searchV2.repository.SearchRepositoryV2;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
 public class SearchServiceV2 {
 
-    private final SearchRepositoryV2 searchRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
     private final ProductRepository productRepository;
 
-    @Transactional
     public void increaseSearchCount(String keyword) {
 
-        SearchV2 search = searchRepository.findByKeyword(keyword)
-                .orElseGet(() -> searchRepository.save(new SearchV2(keyword)));
+        redisTemplate.opsForZSet().incrementScore("search:ranking", keyword, 1);
 
-        search.increaseCount();
+        redisTemplate.expire("search:ranking", Duration.ofHours(1));
     }
 
-    @Cacheable(value = "searchCache", key = "#keyword")
+    @Transactional
     public Slice<ProductResponseDto> findByProductName(String keyword) {
 
         Pageable pageable = PageRequest.of(0,10);
         Slice<Product> products = productRepository.findByProductNameContaining(keyword, pageable);
+
+        increaseSearchCount(keyword);
 
         return products.map(ProductResponseDto::new);
     }
@@ -45,10 +46,12 @@ public class SearchServiceV2 {
     @Transactional(readOnly = true)
     public List<PopularSearchResponseDto> findByPopulation() {
 
-        List<SearchV2> top10 = searchRepository.findTop10ByOrderByCountDesc();
+        Set<ZSetOperations.TypedTuple<Object>> top10 = redisTemplate.opsForZSet().reverseRangeWithScores("search:ranking", 0, 9);
 
-        return IntStream.range(0, top10.size())
-                .mapToObj(i -> new PopularSearchResponseDto(i + 1, top10.get(i).getKeyword()))
+        List<ZSetOperations.TypedTuple<Object>> top10List = top10.stream().toList();
+
+        return IntStream.range(0, top10List.size())
+                .mapToObj(i -> new PopularSearchResponseDto(i + 1, (String) top10List.get(i).getValue()))
                 .toList();
     }
 
