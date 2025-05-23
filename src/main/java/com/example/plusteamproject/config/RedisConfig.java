ef68f76a4cc6dd51b8225815a86f5ccdd3e57b76
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-05-23 10:13:22 +0900

    feat:redis 적용

--- src/main/java/com/example/plusteamproject/config/RedisConfig.java
+++ src/main/java/com/example/plusteamproject/config/RedisConfig.java
@@ -1,4 +1,5 @@
 package com.example.plusteamproject.config;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,29 +9,40 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 @Configuration
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String host;
+
     @Value("${spring.data.redis.port}")
     private int port;
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory,
+                                                       ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-        // ObjectMapper에 JavaTimeModule 등록
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        // GenericJackson2JsonRedisSerializer에 커스텀 ObjectMapper 설정
+
+        // 직렬화 설정
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-        // 키는 문자열로
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        // 값은 JSON으로 직렬화
         template.setValueSerializer(serializer);
         template.setHashValueSerializer(serializer);
+
         return template;
-        //spring boot 2.x 버전 이상은 자동으로 주입됨
+    }
+
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+    //spring boot 2.x 버전 이상은 자동으로 주입됨
 //    @Bean
 //    public RedisConnectionFactory redisConnectionFactory() {
 //        return new LettuceConnectionFactory(host, port);
@@ -42,5 +54,7 @@ public class RedisConfig {
 //        redisTemplate.setConnectionFactory(redisConnectionFactory);
 //        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
 //        return redisTemplate;
-    }
+
 }
+
+
